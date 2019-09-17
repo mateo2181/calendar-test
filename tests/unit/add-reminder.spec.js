c6fb599ae25ee9094b8e2000e2d0b6f51c9ed57e
mateo <mateomerlo@gmail.com>
2019-09-17 19:30:52 -0300

    Fixed add-reminder test

--- tests/unit/add-reminder.spec.js
+++ tests/unit/add-reminder.spec.js
@@ -1,60 +1,54 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import { shallowMount } from '@vue/test-utils'
+import { createLocalVue,shallowMount } from '@vue/test-utils'
 import AddReminder from '@/components/AddReminder.vue'
 Vue.use(Vuex)
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
-describe('AddReminder.vue', () => {
+import Vuelidate from 'vuelidate'
+Vue.use(Vuelidate)
 
-  let store
-  let state
-  let actions
-  beforeEach(() => {
-    state = { 
-      reminders: [] 
-    };
-    actions = {
-      add_reminder: jest.fn()
-    };
-  
-    store = new Vuex.Store({
-      state,
-      actions
-    })
-  })
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+const actions = {
+  add_reminder: jest.fn()
+};
+
+const store = new Vuex.Store({ actions })
+
+describe('AddReminder.vue', () => {
 
   it('add reminder with day, time and city', () => {
     
     // let reminders = [];
     const time = '12';
-    const day = 1;
+    const day = 3;
     const month = 2;
-    const year = 2019;
+    const year = 2018;
     const city = 'London';
     const text = 'Pay Bills!!';
 
     const options = {
-      methods: {
-        validateFields() {
-          return true;
-        }
-      },
-      store
+      store, 
+      localVue
     };
 
     const wrapper = shallowMount(AddReminder, options);
 
     wrapper.find('.btn-show-form').trigger('click');
-    wrapper.find('input[name="day"]').setValue(day);
-    wrapper.find('select[name="month"]').setValue(month);
-    wrapper.find('select[name="year"]').setValue(year);
-    wrapper.find('input[name="time"]').setValue(time);
-    wrapper.find('input[name="city"]').setValue(city);
-    wrapper.find('input[name="text"]').setValue(text);
-    wrapper.find('.btn-create-reminder').trigger('click');
+
+    wrapper.vm.form.day = day;
+    wrapper.vm.form.month = month;
+    wrapper.vm.form.year = year;
+    wrapper.vm.form.time = time;
+    wrapper.vm.form.city = city;
+    wrapper.vm.form.text = text;
+
+    const form = wrapper.find("form");
+    form.trigger("submit");
     expect(actions.add_reminder.mock.calls).toHaveLength(1);
   })
 })
